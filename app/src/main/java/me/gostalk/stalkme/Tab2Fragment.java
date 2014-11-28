7b2172f3bc4dbe68bedace08b0facaa234ba29fd
BenjaminEarley <thebigben92@gmail.com>
2014-11-28 16:49:37 -0500

    new recycler view for friends list plus card views

--- app/src/main/java/me/gostalk/stalkme/Tab2Fragment.java
+++ app/src/main/java/me/gostalk/stalkme/Tab2Fragment.java
@@ -2,31 +2,77 @@ package me.gostalk.stalkme;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+
 import com.shamanland.fab.FloatingActionButton;
 import com.shamanland.fab.ShowHideOnScroll;
 
+import Adapters.CustomRecyclerAdapter;
+
 public class Tab2Fragment extends Fragment {
 
-    private final static String TAG = "FloatingActionButtonBasicFragment";
+    protected RecyclerView mRecyclerView;
+    protected RecyclerView.Adapter mAdapter;
+    protected RecyclerView.LayoutManager mLayoutManager;
+    protected String[] mDataset;
+    private static final String TAG = "RecyclerView2Fragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize dataset, this data would usually come from a local content provider or
+        // remote server.
+        initDataset();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.tab2_view, container, false);
+        rootView.setTag(TAG);
+        // BEGIN_INCLUDE(initializeRecyclerView)
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView2);
+
+        // LinearLayoutManager is used here, this will layout the elements in a similar fashion
+        // to the way ListView would layout elements. The RecyclerView.LayoutManager defines how
+        // elements are laid out.
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new CustomRecyclerAdapter(mDataset);
+        // Set CustomAdapter as the adapter for RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        // END_INCLUDE(initializeRecyclerView)
 
         // Make this {@link Fragment} listen for changes in both FABs.
         FloatingActionButton fab1 = (FloatingActionButton) rootView.findViewById(R.id.fab_1);
-        ListView listView = (ListView) rootView.findViewById(R.id.listView);
-        listView.setOnTouchListener(new ShowHideOnScroll(fab1));
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView2);
+        mRecyclerView.setOnTouchListener(new ShowHideOnScroll(fab1));
 
         return rootView;
     }
 
+    /**
+     * Generates Strings for RecyclerView's adapter. This data would usually come
+     * from a local content provider or remote server.
+     */
+    private void initDataset() {
+        mDataset = new String[60];
+        for (int i=0; i < 60; i++) {
+            mDataset[i] = "This is element #" + i;
+        }
+    }
 }
