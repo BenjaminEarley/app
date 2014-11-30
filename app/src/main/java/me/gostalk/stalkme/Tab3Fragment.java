8557fed98d82d55673fa9a13e3384787e3112cc5
Ryan <rcamp021@odu.edu>
2014-11-30 18:44:54 -0500

    Map Center

--- app/src/main/java/me/gostalk/stalkme/Tab3Fragment.java
+++ app/src/main/java/me/gostalk/stalkme/Tab3Fragment.java
@@ -1,22 +1,35 @@
 package me.gostalk.stalkme;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-        import android.support.v4.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.InflateException;
 import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Tab3Fragment extends Fragment  {
+import java.util.List;
+
+public class Tab3Fragment extends Fragment {
 
     private GoogleMap map;
+    private MapView mapView;
     private static View view;
 
     @Override
@@ -30,20 +43,72 @@ public class Tab3Fragment extends Fragment  {
         }
         try {
             view = inflater.inflate(R.layout.tab3_view, container, false);
+            // Gets the MapView from the XML layout and creates it
+            mapView = (MapView) view.findViewById(R.id.map);
+            mapView.onCreate(savedInstanceState);
+
+            // Gets to GoogleMap from the MapView and does initialization stuff
+            map = mapView.getMap();
+            map.getUiSettings().setMyLocationButtonEnabled(false);
+            map.setMyLocationEnabled(true);
+
+            // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+            try {
+                MapsInitializer.initialize(this.getActivity());
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            CenterMap();
         } catch (InflateException e) {
-        /* map is already there, just return view as it is */
+
+// map is already there, just return view as it is
+            //CenterMap();
         }
+
         return view;
 
     }
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
 
-    private GoogleMap getGoogleMap() {
-        if (map == null && getActivity() != null && getActivity().getSupportFragmentManager()!= null) {
-            SupportMapFragment smf = (SupportMapFragment)getActivity().getSupportFragmentManager().findFragmentById(R.id.map);
-            if (smf != null) {
-                map = smf.getMap();
-            }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
+    private void CenterMap() {
+        //map = getGoogleMap();
+
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        if (location != null)
+
+        {
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(location.getLatitude(), location.getLongitude()), 13));
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                    .zoom(17)                   // Sets the zoom
+                    .bearing(90)                // Sets the orientation of the camera to east
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
         }
-        return map;
     }
 }
