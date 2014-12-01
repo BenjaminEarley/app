88509a3c51020517e442e00a97acac32b3db8567
BenjaminEarley <thebigben92@gmail.com>
2014-12-01 03:32:37 -0500

    Crazy Invisible Maps

--- app/src/main/java/Adapters/CustomRecyclerAdapter2.java
+++ app/src/main/java/Adapters/CustomRecyclerAdapter2.java
@@ -1,6 +1,9 @@
 package Adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +15,9 @@ import android.widget.Toast;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 import me.gostalk.stalkme.R;
 
 /**
@@ -22,6 +28,8 @@ public class CustomRecyclerAdapter2 extends RecyclerView.Adapter<CustomRecyclerA
 
     private String[] mDataSet;
     private Context mContext;
+    private GoogleMap _map;
+    private double _latitude, _longitude;
 
 
 
@@ -44,6 +52,25 @@ public class CustomRecyclerAdapter2 extends RecyclerView.Adapter<CustomRecyclerA
                             "onItemClick - " + getPosition() + " - "
                                     + mTextView.getText().toString() + " - "
                                     + mDataSet[getPosition()], Toast.LENGTH_LONG).show();
+                    GoogleMap.SnapshotReadyCallback callback = new GoogleMap.SnapshotReadyCallback() {
+                        Bitmap bitmap;
+
+                        @Override
+                        public void onSnapshotReady(Bitmap snapshot) {
+                            // TODO Auto-generated method stub
+                            bitmap = snapshot;
+                            try {
+                                FileOutputStream out = new FileOutputStream(mContext.getFilesDir()+"/location.PNG");
+                                bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+
+                    _map.snapshot(callback);
+                    Toast.makeText(mContext,"Longitude: " + _longitude + "& Latitude: " + _latitude, Toast.LENGTH_LONG).show();
+
                 }
             });
         }
@@ -58,9 +85,13 @@ public class CustomRecyclerAdapter2 extends RecyclerView.Adapter<CustomRecyclerA
      *
      * @param dataSet String[] containing the data to populate views to be used by RecyclerView.
      */
-    public CustomRecyclerAdapter2(String[] dataSet, Context context) {
+    public CustomRecyclerAdapter2(double latitude, double logitude,GoogleMap map, String[] dataSet, Context context) {
         mDataSet = dataSet;
         mContext = context;
+        _map = map;
+        _latitude = latitude;
+        _longitude = logitude;
+
     }
 
     // BEGIN_INCLUDE(recyclerViewOnCreateViewHolder)
@@ -91,5 +122,13 @@ public class CustomRecyclerAdapter2 extends RecyclerView.Adapter<CustomRecyclerA
     @Override
     public int getItemCount() {
         return mDataSet.length;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mContext.getFilesDir()+"/location.PNG");
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        mContext.sendBroadcast(mediaScanIntent);
     }
 }
--- app/src/main/java/me/gostalk/stalkme/Tab2Fragment.java
+++ app/src/main/java/me/gostalk/stalkme/Tab2Fragment.java
@@ -1,12 +1,17 @@
 package me.gostalk.stalkme;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +22,12 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 import com.shamanland.fab.FloatingActionButton;
 import com.shamanland.fab.ShowHideOnScroll;
 
@@ -27,7 +38,10 @@ public class Tab2Fragment extends Fragment {
     protected RecyclerView mRecyclerView;
     protected RecyclerView.Adapter mAdapter;
     protected RecyclerView.LayoutManager mLayoutManager;
+    private GoogleMap map;
+    private MapView mapView;
     protected String[] mDataset;
+    double latitude, logitude;
     private static final String TAG = "RecyclerView2Fragment";
 
     @Override
@@ -46,6 +60,34 @@ public class Tab2Fragment extends Fragment {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.tab2_view, container, false);
         rootView.setTag(TAG);
+
+        if (rootView != null) {
+            ViewGroup parent = (ViewGroup) rootView.getParent();
+            if (parent != null)
+                parent.removeView(rootView);
+        }
+        try {
+            // Gets the MapView from the XML layout and creates it
+            mapView = (MapView) rootView.findViewById(R.id.map_invisible);
+            mapView.onCreate(savedInstanceState);
+
+            // Gets to GoogleMap from the MapView and does initialization stuff
+            map = mapView.getMap();
+            map.getUiSettings().setMyLocationButtonEnabled(false);
+            map.setMyLocationEnabled(true);
+
+            // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+            try {
+                MapsInitializer.initialize(this.getActivity());
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            CenterMap();
+        } catch (InflateException e) {
+            // map is already there, just return view as it is
+        }
+
         // BEGIN_INCLUDE(initializeRecyclerView)
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView2);
 
@@ -55,7 +97,7 @@ public class Tab2Fragment extends Fragment {
         mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new CustomRecyclerAdapter2(mDataset, getActivity());
+        mAdapter = new CustomRecyclerAdapter2(latitude, logitude,map, mDataset, getActivity());
         // Set CustomAdapter as the adapter for RecyclerView.
         mRecyclerView.setAdapter(mAdapter);
         // END_INCLUDE(initializeRecyclerView)
@@ -84,6 +126,7 @@ public class Tab2Fragment extends Fragment {
                 builder.show();
             }
         });
+
 
         return rootView;
     }
@@ -121,6 +164,24 @@ private void SetDialog(View view)
 
 }
 
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
     /**
      * Generates Strings for RecyclerView's adapter. This data would usually come
      * from a local content provider or remote server.
@@ -136,5 +197,30 @@ private void SetDialog(View view)
         mDataset = new String[1];
         //if(mDataset[i].toLowerCase().contains(search.toLowerCase()))
             mDataset[0] = "This is friend #" + 7;
+    }
+
+    private void CenterMap() {
+
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        if (location != null)
+
+        {
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(location.getLatitude(), location.getLongitude()), 13));
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                    .zoom(17)                   // Sets the zoom
+                    .bearing(90)                // Sets the orientation of the camera to east
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            latitude = location.getLatitude();
+            logitude = location.getLongitude();
+            map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        }
     }
 }
